27116e948fc55058c3ec6cdb72892fb5725eb58a
User1391 <pye3yh@virginia.edu>
2025-02-11 13:35:14 -0500

    OOOH

--- scrape.py
+++ scrape.py
@@ -14,14 +14,13 @@ logger = logging.getLogger('listing-scraper')
 
 def setup_driver():
     chrome_options = Options()
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--lang=en')
     
-    # Add persistent profile directory
+    # Add the profile directory
     profile_dir = '/home/bitnami/.config/chrome-profile'
     chrome_options.add_argument(f'--user-data-dir={profile_dir}')
     chrome_options.add_argument('--profile-directory=Default')
