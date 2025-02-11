8c408ec7e62e4e8c4305d62f70ef98b6733a7252
User1391 <pye3yh@virginia.edu>
2025-02-11 13:05:23 -0500

    ASSS

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from scrape import extract_listing_data  # Import the function from scrape.py
+import traceback
 
 app = Flask(__name__)
 CORS(app, resources={
